00d6174d1c19a8baa7e53615571afce468ee1aa5
LibbeyKwok <libbeykwok8@gmail.com>
2023-07-13 13:52:26 -0700

    draft!

--- About Me App/ContentView.swift
+++ About Me App/ContentView.swift
@@ -8,14 +8,58 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var fact1 = ""
+    @State private var fact2 = ""
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
+        HStack{
+            VStack{
+                Image("hand")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                Spacer()
+                Image("computer")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                Button("Click for a fun fact")
+                {
+                 fact1="1. My family has 6 people in it "
+                 fact2="2. I like coding"
+                }
+
+                .font(.title2)
+                .buttonStyle(.borderedProminent)
+                .tint(.indigo)
+                Text(fact1)
+                Text(fact2)
+                
+                
+                
+            }
+            VStack(){
+                Text("L")
+                    .font(.largeTitle)
+                    .fontWeight(.heavy)
+                Text("I")
+                    .font(.largeTitle)
+                    .fontWeight(.heavy)
+                Text("B")
+                    .font(.largeTitle)
+                    .fontWeight(.heavy)
+                Text("B")
+                    .font(.largeTitle)
+                    .fontWeight(.heavy)
+                Text("E")
+                    .font(.largeTitle)
+                    .fontWeight(.heavy)
+                Text("Y")
+                    .font(.largeTitle)
+                    .fontWeight(.heavy)
+
+
+            
+            }
+                
+        }.padding()
     }
 }
 
